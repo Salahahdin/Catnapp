68452e1c3786017ae3c2e46769951075dcfd9329
Alex Biedka <alexbiedka@gmail.com>
2022-02-17 18:35:54 +0100

    flask demo app+

--- main.py
+++ main.py
@@ -1,47 +1,27 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
 
 
-import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6.QtGui import QIcon, QAction
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
 
 
-class Example(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-    def initUI(self):
-
-        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit application')
-
-        addCat = QAction(QIcon('adcat.png'), '&Add Cat', self)
-        addCat.setShortcut('Ctrl+Q')
-        addCat.setStatusTip('Add Cat')
-        addCat.triggered.connect(QApplication.instance().quit)
-
-        self.statusBar()
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAct)
-        fileMenu.addAction(addCat)
-
-        self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Simple menu')
-        self.show()
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 
-def main():
-
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec())
-
-
-if __name__ == '__main__':
-    main()
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
